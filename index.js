ef615583bd8699005b74b323101c3fadb0783e83
shubhamsomwanshi11 <shubhamsomwanshi11@gmail.com>
2023-02-22 23:35:58 +0530

    Updated

--- index.js
+++ index.js
@@ -1,59 +1,62 @@
-function SortData() { 
-    const parser = new DOMParser();
-    const xmlString = document.getElementById('inputarea').value.toString();
-    const xmlDoc = parser.parseFromString(xmlString, 'application/xml');
-    const root = xmlDoc.documentElement;
-    const childNodes = Array.from(root.childNodes);
-    childNodes.sort((a, b) => a.tagName.localeCompare(b.tagName));
-    const sortedXmlDoc = new DOMParser().parseFromString('<root></root>', 'application/xml');
-    const sortedRoot = sortedXmlDoc.documentElement;
-    for (const childNode of childNodes) {
-        const sortedParentNode = sortedXmlDoc.createElement(childNode.tagName);
-        const childElements = Array.from(childNode.getElementsByTagName('*'));
-        childElements.sort((a, b) => a.tagName.localeCompare(b.tagName));
-        for (const childElement of childElements) {
-            sortedParentNode.appendChild(childElement.cloneNode(true));
-        }
-        sortedRoot.appendChild(sortedParentNode);
+function sortXML(xml) {
+  for (let i = 0; i < xml.childNodes.length; i++) {
+    const node = xml.childNodes[i];
+    if (node.nodeType === 1 && node.hasChildNodes()) {
+      const sortedChildNodes = Array.from(node.childNodes)
+        .filter(childNode => childNode.nodeType === 1)
+        .sort((a, b) => a.nodeName.localeCompare(b.nodeName));
+      sortedChildNodes.forEach(childNode => node.appendChild(childNode));
+      sortXML(node);
     }
-    const serializer = new XMLSerializer();
-    const sortedXmlString = serializer.serializeToString(sortedXmlDoc);
-    var formatted = formatXml(sortedXmlString)
+  }
+  return xml;
+}
+
+function SortData() {
+  const xmlString = document.getElementById('inputarea').value.toString();
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(xmlString, "application/xml");
+  if (xml.getElementsByTagName("parsererror").length > 0) {
+    alert("Invalid XML");
+  }
+  else {
+    const sortedXml = sortXML(xml);
+    console.log(sortedXml.documentElement.outerHTML);
+    var formatted = formatXml(sortedXml.documentElement.outerHTML);
     document.getElementById('outputarea').value = formatted;
-    highlightXmlTags('outputarea');
+  }
 }
 
 function formatXml(xml) {
-    var formatted = '';
-    var reg = /(>)(<)(\/*)/g;
-    xml = xml.replace(reg, '$1\r\n$2$3');
-    var pad = 0;
-    xml.split('\r\n').forEach(function(node, index) {
-      var indent = 0;
-      if (node.match(/.+<\/\w[^>]*>$/)) {
-        indent = 0;
-      } else if (node.match(/^<\/\w/)) {
-        if (pad != 0) {
-          pad -= 1;
-        }
-      } else if (node.match(/^<\w[^>]*[^/]>.*$/)) {
-        indent = 1;
-      } else {
-        indent = 0;
+  var formatted = '';
+  var reg = /(>)(<)(\/*)/g;
+  xml = xml.replace(reg, '$1\r\n$2$3');
+  var pad = 0;
+  xml.split('\r\n').forEach(function (node, index) {
+    var indent = 0;
+    if (node.match(/.+<\/\w[^>]*>$/)) {
+      indent = 0;
+    } else if (node.match(/^<\/\w/)) {
+      if (pad != 0) {
+        pad -= 1;
       }
-      var padding = '';
-      for (var i = 0; i < pad; i++) {
-        padding += '  ';
-      }
-      formatted += padding + node + '\r\n';
-      pad += indent;
-    });
-    return formatted;
-  }
+    } else if (node.match(/^<\w[^>]*[^/]>.*$/)) {
+      indent = 1;
+    } else {
+      indent = 0;
+    }
+    var padding = '';
+    for (var i = 0; i < pad; i++) {
+      padding += '  ';
+    }
+    formatted += padding + node + '\r\n';
+    pad += indent;
+  });
+  return formatted;
+}
 
-  function highlightXmlTags(textBoxId) {
-    const textBox = document.getElementById(textBoxId);
-    hljs.highlightBlock(textBox);
-  }
-  
-  
+function highlightXmlTags(textBoxId) {
+  const textBox = document.getElementById(textBoxId);
+  hljs.highlightBlock(textBox);
+}
+
